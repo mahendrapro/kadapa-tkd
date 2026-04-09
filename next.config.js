6ba585083755b9980640d5a4e2e9de6f62075ca6
mahendrapro <mahendra0123456789@gmail.com>
2026-04-09 15:41:22 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  distDir: 'out',
+  trailingSlash: true,
+  images: { unoptimized: true },
 };
-
 module.exports = nextConfig;
